8788df5c4a29e38ce0f1f417d5fb069d9398a78f
Chai Phonbopit <phonbopit@gmail.com>
2015-09-05 10:49:57 +0700

    add gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
 var gulp = require('gulp');
 
-var webserver = require('gulp-webserver');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app/',
+    port: 4000
+  });
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/assets/sass/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('clean', function(callback) {
+  del(['dist'], callback);
+});
